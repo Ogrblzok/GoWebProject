1f700837983342f62cc75aae95573f0e0d4d656c
Maxim Skoromykin <maksimskoro25@gmail.com>
2025-09-17 22:33:36 +0500

    first test commit

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	text := "Here's my template!\n"
+	tmpl, err := template.New("test").Parse(text)
+	check(err)
+	err = tmpl.Execute(os.Stdout, nil)
+	check(err)
+	html, err := template.ParseFiles("templates/view.html")
+	check(err)
+	err = html.Execute(writer, nil)
+	check(err)
+}
 
 func main() {
-	fmt.Println("smth")
+	http.HandleFunc("/guestbook", viewHandler)
+	err := http.ListenAndServe("localhost:8080", nil)
+	log.Fatal(err)
 }
